db9f3e19866463051d3b447ac89a93d801735b1e
Aamya Chauhan <93673911+aamya2004@users.noreply.github.com>
2023-06-11 23:56:28 +0530

    Update bubbleSort.cpp

--- Recursion/bubbleSort.cpp
+++ Recursion/bubbleSort.cpp
@@ -1,1 +1,25 @@
-
+//BubbleSort using recursion
+#include<bits/stdc++.h>
+using namespace std;
+void bubbleSort(int *arr,int size){
+    if(size==0||size==1){
+        return ;
+    }
+    for(int i=0;i<size;i++){
+      if(arr[i]>arr[i+1]){
+          swap(arr[i],arr[i+1]);
+      }  
+    }
+    bubbleSort(arr,size-1);
+    
+}
+int main(){
+    //static input
+    int arr[5]={2,1,5,4};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    bubbleSort(arr,n);
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
