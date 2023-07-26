9b83397ba8738a2ac5e9bdf8eda5fc018f7d9411
Aamya Chauhan <93673911+aamya2004@users.noreply.github.com>
2023-07-26 16:47:00 +0530

    Tricky approach without sorting

---
+++ Sort012.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h> 
+void sort012(int *arr, int n)
+{
+   int low = 0;
+   int high = n-1;
+   int mid = 0;
+   while(mid<=high){
+      switch(arr[mid])
+      {
+         case 0:
+            swap(arr[mid++],arr[low++]);
+            break;
+         case 1:
+            mid++;
+            break;
+         case 2:
+            swap(arr[mid],arr[high--]);
+            break;
+      }
+   }
+
+}
